178a950d718a14b59f2b11eff1dc1b983982f8ae
Iresh Maharjan <iresh.maharjan@gmail.com>
2024-12-18 02:16:09 +0545

    fixed card style

--- app/(app)/@viewer/component/glass-morphism-card.tsx
+++ app/(app)/@viewer/component/glass-morphism-card.tsx
@@ -11,9 +11,9 @@ export function GlassmorphismCard({
 }) {
   return (
     <div
-      className={`${className} hover:lg:shadow-lg hover:lg:border hover:lg:border-slate-600 hover:lg:bg-slate-600 hover:lg:bg-clip-padding hover:lg:backdrop-filter hover:lg:backdrop-blur hover:lg:bg-opacity-10 hover:lg:backdrop-saturate-100 hover:lg:backdrop-contrast-100 hover:lg:p-10 rounded-lg`}
+      className={`${className} hover:lg:shadow-lg hover:lg:border hover:lg:border-slate-600 hover:lg:bg-slate-600 hover:lg:bg-clip-padding hover:lg:backdrop-filter hover:lg:backdrop-blur hover:lg:bg-opacity-10 hover:lg:backdrop-saturate-100 hover:lg:backdrop-contrast-100 hover:lg:p-6 rounded-lg`}
     >
-      <div className="hover:lg:p-6 transition-transform duration-300 ease-in-out hover:scale-105">
+      <div className="transition-transform duration-300 ease-in-out">
         {children}
       </div>
     </div>
